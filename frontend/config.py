95d5f12446057160005cbb6f84bf251d1e250692
Carl <carlsebastian.e.barcelona@gmail.com>
2026-06-07 19:34:54 +0800

    POST requests API included in config for frontend

--- frontend/config.py
+++ frontend/config.py
@@ -3,3 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 API_BASE = os.getenv("API_BASE", "http://localhost:8000")
+
+# For POST
+FCFS_API = f"{API_BASE}/schedule/fcfs"
+SJF_NP_API = f"{API_BASE}/schedule/sjf_np"
+SJF_PRE_API = f"{API_BASE}/schedule/sjf_pre"
+PRIORITY_PRE_API = f"{API_BASE}/schedule/priority_pre"
+PRIORITY_NP_API = f"{API_BASE}/schedule/priority_np"
+ROUND_ROBIN_API = f"{API_BASE}/schedule/round_robin"
