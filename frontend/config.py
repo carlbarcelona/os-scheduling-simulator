1e768c6a5699f24256095172bcb3532ff982ef86
Carl <carlsebastian.e.barcelona@gmail.com>
2026-06-16 15:56:56 +0800

    Schema and backend integration for mass storage algorithms

--- frontend/config.py
+++ frontend/config.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 API_BASE = os.getenv("API_BASE", "http://localhost:8000")
 
-# For POST
+# -- For POST -- 
+# CPU Scheduling APIs
 FCFS_API = f"{API_BASE}/schedule/fcfs"
 SJF_NP_API = f"{API_BASE}/schedule/sjf_np"
 SJF_PRE_API = f"{API_BASE}/schedule/sjf_pre"
 PRIORITY_PRE_API = f"{API_BASE}/schedule/priority_pre"
 PRIORITY_NP_API = f"{API_BASE}/schedule/priority_np"
 ROUND_ROBIN_API = f"{API_BASE}/schedule/round_robin"
+MLFQ_API = f"{API_BASE}/schedule/mlfq" # Empty
